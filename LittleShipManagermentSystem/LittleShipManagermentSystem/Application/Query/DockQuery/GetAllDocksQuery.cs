0b1a57cd9e8b080954c1c3369aa48235191cebb2
Gokay DINC <gokaydinc2000@gmail.com>
2023-08-25 10:30:21 +0300

    ..

--- LittleShipManagermentSystem/LittleShipManagermentSystem/Application/Query/DockQuery/GetAllDocksQuery.cs
+++ LittleShipManagermentSystem/LittleShipManagermentSystem/Application/Query/DockQuery/GetAllDocksQuery.cs
@@ -1,4 +1,6 @@
-﻿using LittleShipManagermentSystemApi.Application.Query.DockQuery.Request;
+﻿using ListtleShipManagementSystemDomain.Entities;
+using LittleShipManagementSystemData.Repositories.Abstract;
+using LittleShipManagermentSystemApi.Application.Query.DockQuery.Request;
 using LittleShipManagermentSystemApi.Application.Query.DockQuery.Response;
 using MediatR;
 
@@ -6,8 +8,7 @@ namespace LittleShipManagermentSystemApi.Application.Query.DockQuery
 {
     public class GetAllDocksQuery : IRequestHandler<GetAllDocksRequest, GetAllDocksResponse>
     {
-
-
+        private readonly IRepository<Dock> _dockRepository;
 
 
         public Task<GetAllDocksResponse> Handle(GetAllDocksRequest request, CancellationToken cancellationToken)
